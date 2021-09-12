af975bd2accccdaa39e8c5a69a901662f52a69e3
wil707 <76878595+wil707@users.noreply.github.com>
2021-09-12 18:28:23 +0100

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,4 @@
+#This is a comment!
+Colour,Animal='Blue','monkey'
+print("The " +Animal+ " turned " +Colour+ ".")
+#The code above shows two variables in one sentence.
---
+++ Activity 3.py
@@ -0,0 +1,4 @@
+#This is a comment!
+Adjective,Animal,Number='hilarious','monkeys','4'
+print("There were " +Number+ " blue " +Animal+ ", they were " +Adjective+ ".")
+#The code above shows 3 variables in one sentence.
